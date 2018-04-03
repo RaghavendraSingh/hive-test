f39b88cf08b702578be9db847a525925b8fe9c09
Raghavendra Singh <raghavendra.singh@inmobi.com>
2018-04-03 19:29:40 +0530

    more changes

--- src/main/java/HiveCreateTable.java
+++ src/main/java/HiveCreateTable.java
@@ -1,7 +1,10 @@
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.tools.ant.taskdefs.Classloader;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -21,8 +24,9 @@ public class HiveCreateTable extends URLClassLoader{
     super(urls, parent);
   }
 
-  public static void main(String[] args) throws SQLException, ClassNotFoundException, MalformedURLException,
-      IllegalAccessException, InstantiationException {
+  public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException,
+          IllegalAccessException, InstantiationException {
+
     System.out.println(JARS_DIR);
     System.out.println(new Path(JARS_DIR).toUri().toURL());
     System.out.println(getFilesInPath(new Path(JARS_DIR).toUri().toURL()));
@@ -30,31 +34,48 @@ public class HiveCreateTable extends URLClassLoader{
 
     ClassLoader classloader = loadJar();
     Thread.currentThread().setContextClassLoader(classloader);
-
     Class.forName(driverName);
 
-
-
-
      //get connection
-    Connection con = DriverManager.getConnection("jdbc:hive2://plhvs4001.grid.dfw1.inmobi.com:10000/default", "", "");
+    String url = args[0];
+    String port = args[1];
+    String db = args[2];
+    String hiveServerPrincipal = args[3];
+
+    Configuration conf = new Configuration();
+    conf.set("hadoop.security.authentication", "Kerberos");
+    UserGroupInformation.setConfiguration(conf);
+
+    // login the user if a keytab file is present
+    // otherwise, assume kinit was already done
+    if(args.length >= 5) {
+      String loginUser = args[4];
+      String keytabFile = args[5];
+      UserGroupInformation.loginUserFromKeytab(loginUser, keytabFile);
+    }
+
+
+    Connection con = DriverManager.getConnection("jdbc:hive2://"+url+":"+port+"/"+db+";principal="+hiveServerPrincipal+"", "", "");
 
     // create statement
     Statement stmt = con.createStatement();
 
     // execute statement
-//    stmt.executeQuery("CREATE TABLE IF NOT EXISTS "
-//        +" employee ( eid int, name String, "
-//        +" salary String, destignation String)"
-//        +" COMMENT ‘Employee details’"
-//        +" ROW FORMAT DELIMITED"
-//        +" FIELDS TERMINATED BY ‘\t’"
-//        +" LINES TERMINATED BY ‘\n’"
-//        +" STORED AS TEXTFILE;");
+    ResultSet createTable = stmt.executeQuery("CREATE TABLE IF NOT EXISTS "
+        +" employee ( eid int, name String, "
+        +" salary String, destignation String)"
+        +" COMMENT ‘Employee details’"
+        +" ROW FORMAT DELIMITED"
+        +" FIELDS TERMINATED BY ‘\t’"
+        +" LINES TERMINATED BY ‘\n’"
+        +" STORED AS TEXTFILE;");
+    while (createTable.next()) {
+      System.out.println(createTable.getString(1));
+    }
 
-    ResultSet show_tables = stmt.executeQuery("show tables");
-    while (show_tables.next()) {
-      System.out.println(show_tables.getString(1));
+    ResultSet showTables = stmt.executeQuery("show tables");
+    while (showTables.next()) {
+      System.out.println(showTables.getString(1));
     }
 
     System.out.println("Table employee created.");
